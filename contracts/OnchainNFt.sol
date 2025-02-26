67333806f9bc8a00f79b1da7abe751d18e4c8599
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2025-02-26 19:03:39 +0100

    onchain nft

---
+++ contracts/OnchainNFt.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract OnchainNft is ERC721URIStorage {
+
+    uint256 public tokenCounter;
+    constructor() ERC721("OnchainNFT", "ONFT") {
+        tokenCounter = 0;
+    }
+
+    
+    function mintNft(string memory imageURI) public {
+        _safeMint(msg.sender, tokenCounter);
+        _setTokenURI(tokenCounter, imageURI);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721URIStorage) returns (string memory) {
+        string memory imageURI = ERC721URIStorage.tokenURI(tokenId);
+
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name": "', 
+                            name(), 
+                            '", "description": "An NFT that reflects the owners mood", ',
+                            '"attributes": [{"trait_type": "moodiness", "value": 100}], ',
+                            '"image": "', 
+                            imageURI, 
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    
+}
---
+++ scripts/nft.js
@@ -0,0 +1,26 @@
+const hre = require("hardhat");
+
+async function main() {
+    const OnChainNFt = await hre.ethers.getContractFactory("OnchainNft");
+
+    const onChainNFt = await OnChainNFt.deploy();
+
+    await onChainNFt.waitForDeployment();
+
+    const nftAddress = await onChainNFt.getAddress();
+
+    console.log(`Deploy contract to ${nftAddress.toString()}`);
+
+    const [owner, otherAccount] = await ethers.getSigners();
+    const svgBase = "PHN2ZyBoZWlnaHQ9IjEwMCIgd2lkdGg9IjEwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8Y2lyY2xlIHI9IjQ1IiBjeD0iNTAiIGN5PSI1MCIgc3Ryb2tlPSJncmVlbiIgc3Ryb2tlLXdpZHRoPSIzIiBmaWxsPSJyZWQiIC8+CiAgPHRleHQ+SSB3ZWIzYnJpZGdlPC90ZXh0Pgo8L3N2Zz4g"
+
+    const mintNFt = await onChainNFt.mintNft(svgBase)
+
+    await mintNFt.wait();
+    console.log("NFT minted!", mintNFt);
+}
+
+main().catch((error) => {
+    console.log(error);
+    process.existCode = 1;
+}) 
